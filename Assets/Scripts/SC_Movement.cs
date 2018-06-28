4925cce813b9cca00e1ac0ef75054f4a892453af
Ricardo <ricardo95pascual@gmail.com>
2018-06-28 20:28:02 +0200

    Añadido salto, con altura que cambia si mantienes pulsado más o menos

--- Assets/Scripts/SC_Movement.cs
+++ Assets/Scripts/SC_Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SC_Movement : MonoBehaviour {
 
     bool _grounded = true;
+    bool _canSecondJump = true;
     
     public float _speedGroundAcceleration;
     public float _speedGroundBrakeTo0 = 0.5f;
@@ -19,7 +20,10 @@ public class SC_Movement : MonoBehaviour {
     float _speedBrakeTo0Force = 0.5f;
 
     Rigidbody _rb;
-    
+
+    public float _jumpSpeed0 = 8f;
+    public float _jumpBrakeSpeed = 3f;
+
 
 	// Use this for initialization
 	void Start () {
@@ -31,9 +35,55 @@ public class SC_Movement : MonoBehaviour {
 
 
         CheckGrounded();
+       
+        //Apply the movement
+        if (Input.GetAxisRaw("Horizontal") != 0f)
+        {
+            DoAcceleration();
+        }
+        else DoAcceleration0();
+
         
-        
-        //Check if grounded
+
+        //Jump
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log(Input.GetAxis("Jump"));
+            if (_grounded)
+            {
+                Jump(_jumpSpeed0);
+            }
+            else if (_canSecondJump)
+            {
+
+            }
+        }
+
+        if (Input.GetKeyUp("space"))
+        {
+            if (_rb.velocity.y > _jumpBrakeSpeed)
+            {
+                Vector3 rbVel = _rb.velocity;
+                rbVel.y = _jumpBrakeSpeed;
+                _rb.velocity = rbVel;
+            }
+        }
+
+    }
+
+    void CheckGrounded()
+    {
+        RaycastHit hit;
+        Ray groundRay = new Ray(transform.position, Vector3.down);
+
+        if (Physics.Raycast(groundRay, out hit, 1.01f))
+        {
+            _grounded = true;
+        }
+        else _grounded = false;
+
+        //Debug.Log(_grounded);
+        //Set ground depending stats
         if (_grounded)
         {
             _speedAcceleration = _speedGroundAcceleration;
@@ -44,18 +94,6 @@ public class SC_Movement : MonoBehaviour {
             _speedAcceleration = _speedAirAcceleration;
             _speedBrakeTo0Force = _speedAirBrakeTo0;
         }
-        
-        //Apply the movement
-        if (Input.GetAxisRaw("Horizontal") != 0f)
-        {
-            DoAcceleration();
-        }
-        else DoAcceleration0();
-    }
-
-    void CheckGrounded()
-    {
-
     }
 
     void DoAcceleration()
@@ -77,5 +115,12 @@ public class SC_Movement : MonoBehaviour {
     {
         Vector3 rbVel = _rb.velocity;
         rbVel.x = Mathf.Lerp(rbVel.x, 0, _speedBrakeTo0Force);
+    }
+
+    void Jump(float speed0)
+    {
+        Vector3 rbVel = _rb.velocity;
+        rbVel.y = speed0;
+        _rb.velocity = rbVel;
     }
 }
