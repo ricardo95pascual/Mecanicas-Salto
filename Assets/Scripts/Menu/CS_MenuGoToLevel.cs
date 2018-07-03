b3c549f818af563b00feb97889179f71a97a0eb3
Ricardo <ricardo95pascual@gmail.com>
2018-07-04 01:19:54 +0200

    Niveles. Cámara fix (?)

---
+++ Assets/Scripts/Menu/CS_MenuGoToLevel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CS_MenuGoToLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void LoadLevel4()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void LoadLevel5()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void LoadLevel6()
+    {
+        SceneManager.LoadScene(7);
+    }
+
+}
