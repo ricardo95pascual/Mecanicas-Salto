a4a31a80c405fc065ab63ef656da2310a84d0871
Ricardo <ricardo95pascual@gmail.com>
2018-07-03 19:49:14 +0200

    Añadido el trigger de siguiente nivel

---
+++ Assets/Scripts/Logic/CS_Logic_EndLevel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CS_Logic_EndLevel : MonoBehaviour {
+
+    //public string _nextScene;
+
+    public bool _goToMenu = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<CS_PlayerLife>())
+        {
+            if (_goToMenu)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            
+            //Application.LoadLevel(0);
+        }
+    }
+
+
+}
