88f91851e68f8e94ada0450a1c12c5b65b800a5b
Ricardo <ricardo95pascual@gmail.com>
2018-07-03 02:30:06 +0200

    Añadido script que HARÁ que el jugador muera

---
+++ Assets/Scripts/CS_PlayerLife.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_PlayerLife : MonoBehaviour {
+
+    public float _spawmDelay = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<SC_IsEnemy>())
+        {
+            //Morir
+        }
+    }
+
+    void Death()
+    {
+        //Cómo morir
+    }
+}
